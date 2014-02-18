16c6927c41a21aefc9c48748e4c69b8a97e51ef3
Phil OSx <philip.bulsink@uottawa.ca>
2014-02-18 14:08:27 -0500

    Initial Commit

---
+++ profile_plotter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+#Profile Plotter
+#Programmer: Philip Bulsink
+#Licence: BSD
+#Plots reaction profiles using matplotlib by reading in energies from a file.
+#See the readme.md file for more information
+
---
+++ profile_plotter_helpers_test.py
@@ -0,0 +1,2 @@
+#!/usr/bin/env python
+#profile_plotter_helpers_test
